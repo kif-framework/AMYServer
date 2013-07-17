8f1639090d99551e1b999d5a6bbb03108ccc0f92
Brian Nickel <brian.nickel@gmail.com>
2013-07-16 17:29:06 -0700

    Initial Commit

---
+++ AMYServer/AMYServer.h
@@ -0,0 +1,13 @@
+//
+//  AMYServer.h
+//  AMYServer
+//
+//  Created by Brian Nickel on 7/16/13.
+//  Copyright (c) 2013 Brian Nickel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AMYServer : NSObject
+
+@end
---
+++ AMYServer/AMYServer.m
@@ -0,0 +1,13 @@
+//
+//  AMYServer.m
+//  AMYServer
+//
+//  Created by Brian Nickel on 7/16/13.
+//  Copyright (c) 2013 Brian Nickel. All rights reserved.
+//
+
+#import "AMYServer.h"
+
+@implementation AMYServer
+
+@end
